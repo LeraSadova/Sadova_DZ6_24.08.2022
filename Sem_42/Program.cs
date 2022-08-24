3071b0af64ccbea332fd317a8e23894f5724048e
Садова Валерия Андреевна <SadovaVA@vseinstrumenti.ru>
2022-08-24 10:55:44 +0300

    решение задач с семинара

--- Sem_42/Program.cs
+++ Sem_42/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.Clear();
+System.Console.Write("Введите десятичное чиcло -> ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int TransToBinary(int num)
+{
+    int result = 0;
+    int i = 1;
+
+    while (num > 0)
+    {
+        result += num % 2 * i;
+        num /= 2;
+        i *= 10;
+    }
+
+    return result;
+
+}
+
+int transToBinary = TransToBinary(number);
+
+Console.WriteLine($"Бинарное -> {transToBinary}");
